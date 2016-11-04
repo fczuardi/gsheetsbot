4e1e22f644fc89c820dd32a42983522525e5d790
Fabricio C Zuardi <fabricio@fabricio.org>
2016-11-04 16:59:17 -0200

    log all telegram updates to a configurable log file

--- src/bot.js
+++ src/bot.js
@@ -1,12 +1,14 @@
 const Telegraf = require('telegraf');
 const config = require('./config');
 const debugMiddleware = require('./middlewares/debug');
+const logMiddleware = require('telegraf-logfile');
 const configMiddleware = require('./middlewares/config');
 const userMiddleware = require('./middlewares/user');
 const commands = require('./commands');
 
 const bot = new Telegraf(config.telegram.token);
 bot.use(debugMiddleware);
+bot.use(logMiddleware(config.log));
 bot.use(configMiddleware);
 bot.use(userMiddleware);
 Object.keys(commands).forEach(name => {
