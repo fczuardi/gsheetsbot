076fe2ddfacad6a3feb40f7849e3c256945db359
Fabricio C Zuardi <fabricio@fabricio.org>
2016-11-05 12:47:01 -0200

    parses handlebars using previous answers as variables

--- src/commands/signup.js
+++ src/commands/signup.js
@@ -1,3 +1,5 @@
+const extend = require('xtend');
+const Handlebars = require('handlebars');
 const tgs = require('telegraf-googlesheets');
 const config = require('../config');
 const loadTable = require('../middlewares/load');
@@ -6,9 +8,12 @@ const addUserStatus = require('../middlewares/userStatus');
 const loadQuestions = loadTable(config.sheets.user.questions);
 const loadUserStatus = loadTable(config.sheets.user.status);
 
+const firstName = name => (name ? name.split(' ')[0] : '');
+
+Handlebars.registerHelper('firstName', firstName);
+
 const signup = (ctx, next) => {
     console.log('ctx.state.sheets', ctx.state.sheets);
-    console.log(ctx.session);
     const userAnswers = ctx.session.answers || [];
     const sheetName = tgs.getSheetName(config.sheets.user.questions);
     const questions = ctx.state.sheets[sheetName];
@@ -17,9 +22,14 @@ const signup = (ctx, next) => {
         ctx.session.awaitingInput = null; // eslint-disable-line
         return ctx.reply('TBD form completed');
     }
-    ctx.session.awaitingInput = 'signup'; // eslint-disable-line 
+    const template = Handlebars.compile(questions[userAnswers.length][2]);
+    const answersObj = userAnswers.reduce((prev, answer, index) => {
+        const varName = questions[index][0];
+        return extend(prev, { [varName]: answer });
+    }, {});
+    ctx.session.awaitingInput = 'signup'; // eslint-disable-line
     ctx.session.answers = userAnswers; // eslint-disable-line
-    ctx.replyWithMarkdown(questions[userAnswers.length][2]);
+    ctx.replyWithMarkdown(template(answersObj));
     return next();
 };
 
