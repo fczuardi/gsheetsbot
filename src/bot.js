59ed6535d0a64329165abe6c29264c6157510d38
Fabricio C Zuardi <fabricio@fabricio.org>
2016-11-05 02:31:28 -0200

    ask all questions and wait for user input on each

--- src/bot.js
+++ src/bot.js
@@ -7,6 +7,8 @@ const userMiddleware = require('./middlewares/user');
 const commands = require('./commands');
 
 const bot = new Telegraf(config.telegram.token);
+// session in memory (ctx.session)
+bot.use(Telegraf.memorySession());
 // output debug logs to the screen
 bot.use(debugMiddleware);
 // log all telegram updates to a log file
@@ -19,6 +21,29 @@ bot.use(userMiddleware);
 // setup all /commandName commands see src/commands/index.js
 Object.keys(commands).forEach(name => {
     bot.command(name, ...commands[name]);
+});
+
+bot.on('text', (ctx, next) => {
+    const { update, updateType } = ctx;
+    if (updateType !== 'message') {
+        return next();
+    }
+    const { text, entities } = update.message;
+    if (entities && entities[0].type === 'bot_command') {
+        return next();
+    }
+    const awaitingInput = ctx.session.awaitingInput;
+    if (awaitingInput) {
+        switch (awaitingInput) {
+        case 'signup':
+            ctx.session.answers.push(text);
+            break;
+        default:
+            break;
+        }
+        return Telegraf.compose(commands[awaitingInput])(ctx, next);
+    }
+    return next();
 });
 bot.startPolling();
 
--- src/commands/signup.js
+++ src/commands/signup.js
@@ -1,3 +1,4 @@
+const tgs = require('telegraf-googlesheets');
 const config = require('../config');
 const loadTable = require('../middlewares/load');
 const addUserStatus = require('../middlewares/userStatus');
@@ -7,6 +8,18 @@ const loadUserStatus = loadTable(config.sheets.user.status);
 
 const signup = (ctx, next) => {
     console.log('ctx.state.sheets', ctx.state.sheets);
+    console.log(ctx.session);
+    const userAnswers = ctx.session.answers || [];
+    const sheetName = tgs.getSheetName(config.sheets.user.questions);
+    const questions = ctx.state.sheets[sheetName];
+    const currentQuestion = userAnswers.length;
+    if (currentQuestion === questions.length) {
+        ctx.session.awaitingInput = null; // eslint-disable-line
+        return ctx.reply('TBD form completed');
+    }
+    ctx.session.awaitingInput = 'signup'; // eslint-disable-line 
+    ctx.session.answers = userAnswers; // eslint-disable-line
+    ctx.reply(questions[userAnswers.length][2]);
     return next();
 };
 
